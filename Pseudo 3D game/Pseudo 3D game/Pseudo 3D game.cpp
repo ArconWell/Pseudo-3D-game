95aecf677d99ce90b7aeceb5a3b54b9a1ef5d093
Alexander <arcon595@gmail.com>
2020-03-27 15:47:08 +0300

    Добавил обработку клавиш управления

--- Pseudo 3D game/Pseudo 3D game/Pseudo 3D game.cpp
+++ Pseudo 3D game/Pseudo 3D game/Pseudo 3D game.cpp
@@ -1,8 +1,6 @@
-﻿// Pseudo 3D game.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 #include <Windows.h>
+#include <chrono>
 
 using namespace std;
 
@@ -19,49 +17,73 @@ int mapWidth = 16;//ширина игрового поля
 float FOV = 3.14159 / 3;//угол обзора (поле видимости)
 float depth = 30.0f;//максимальная дистанция обзора
 
+auto tp1 = chrono::system_clock::now(); // Переменные для подсчета
+auto tp2 = chrono::system_clock::now(); // пройденного времени
+
 int main()
 {
-    //7 строк ниже представляют собой конструкцию, выводящую данные в консоль. Cout не используем, так как он слишком медленный (без понятия как эта конструкция работает)
-    wchar_t* screen = new wchar_t[screenWidth * screenHeight + 1]; // Массив для записи в буфер
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); // Буфер экрана
-    SetConsoleActiveScreenBuffer(hConsole); // Настройка консоли
-    DWORD dwBytesWritten = 0; // Для дебага
+	//7 строк ниже представляют собой конструкцию, выводящую данные в консоль. Cout не используем, так как он слишком медленный (без понятия как эта конструкция работает)
+	wchar_t* screen = new wchar_t[screenWidth * screenHeight + 1]; // Массив для записи в буфер
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL); // Буфер экрана
+	SetConsoleActiveScreenBuffer(hConsole); // Настройка консоли
+	DWORD dwBytesWritten = 0; // Для дебага
 
-    screen[screenWidth * screenHeight] = '\0';  // Последний символ - окончание строки
-    WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0, 0 }, &dwBytesWritten); // Запись в буфер
+	screen[screenWidth * screenHeight] = '\0';  // Последний символ - окончание строки
+	WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0, 0 }, &dwBytesWritten); // Запись в буфер
 
-    wstring map;//wstring - "широкие", локализованные символы
-    map += L"################";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"#..............#";
-    map += L"################";
+	wstring map;//wstring - "широкие", локализованные символы
+	map += L"################";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"#..............#";
+	map += L"################";
 
-    while (1)
-    {
+	while (1)//цикл игры
+	{
+		tp2 = chrono::system_clock::now();//в этих 4 строках рассчитываем время цикла
+		chrono::duration <float> elapsedTime = tp2 - tp1;
+		tp1 = tp2;
+		float fElapsedTime = elapsedTime.count();
 
-    }
-    return 0;
+
+		if (GetAsyncKeyState((unsigned short)'W') & 0x8000)//клавишей W идём вперёд
+		{
+			if (map[(int)playerY * mapWidth + (int)playerX] != '#')//если ячейка, куда хотим переместиться не является стеной
+			{
+				playerX += sinf(playerA);//sinf точнее sin
+				playerY += cosf(playerA);
+			}
+		}
+
+		if (GetAsyncKeyState((unsigned short)'S') & 0x8000)//клавишей S идём назад
+		{
+			if (map[(int)playerY * mapWidth + (int)playerX] != '#')
+			{
+				playerX -= sinf(playerA);
+				playerY -= cosf(playerA);
+			}
+		}
+
+		if (GetAsyncKeyState((unsigned short)'A') & 0x8000)//клавишей A поворачиваем по часовой стрелке
+		{
+			playerA -= 1.5f * fElapsedTime;
+		}
+
+		if (GetAsyncKeyState((unsigned short)'D') & 0x8000)//клавишей D поворачиваем против часовой стрелке
+		{
+			playerA += 1.5f * fElapsedTime;
+		}
+	}
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
